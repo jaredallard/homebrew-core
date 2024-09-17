893e1ce8a46544047b71604e8111ad7990c36f29
Rui Chen <rui@chenrui.dev>
2024-09-17 10:07:38 -0400

    cmix 21
    autobump: add cmix
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/c/cmix.rb
+++ Formula/c/cmix.rb
@@ -1,9 +1,10 @@
 class Cmix < Formula
   desc "Data compression program with high compression ratio"
   homepage "https://www.byronknoll.com/cmix.html"
-  url "https://github.com/byronknoll/cmix/archive/refs/tags/v20.tar.gz"
-  sha256 "a95b0d7430d61b558731e7627f41e170cb7802d1a8a862f38628f8d921dc61b2"
+  url "https://github.com/byronknoll/cmix/archive/refs/tags/v21.tar.gz"
+  sha256 "c0ff50f24604121bd7ccb843045c0946db1077cfb9ded10fe4c181883e6dbb42"
   license "GPL-3.0-or-later"
+  head "https://github.com/byronknoll/cmix.git", branch: "master"
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_sequoia:  "8eebb90a33656eaff7664633fa0422d594d6c5e3fd2c8270e9831516844b610a"
@@ -16,8 +17,14 @@ class Cmix < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "0092c27accca6642f12d8c0d79423e5722dd17cbd20a93c194f231999d0e8655"
   end
 
+  # patch makefile, upstream pr ref, https://github.com/byronknoll/cmix/pull/69
+  patch do
+    url "https://github.com/byronknoll/cmix/commit/702022a974cbf7906bcbaed898f1de95d3cbb32d.patch?full_index=1"
+    sha256 "62143fadb5dda1024b0d51c1bb86263eb15d842193e02550a65924b3ac86c28a"
+  end
+
   def install
-    system "make"
+    system "make", "CXX=#{ENV.cxx}"
     bin.install "cmix"
   end
 
