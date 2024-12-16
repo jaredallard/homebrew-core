d99856218b08ae97e445cb73a706883d6958c238
Rui Chen <rui@chenrui.dev>
2024-12-16 16:53:44 -0500

    fb303 2024.12.23.00
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/f/fb303.rb
+++ Formula/f/fb303.rb
@@ -1,8 +1,8 @@
 class Fb303 < Formula
   desc "Thrift functions for querying information from a service"
   homepage "https://github.com/facebook/fb303"
-  url "https://github.com/facebook/fb303/archive/refs/tags/v2024.12.02.00.tar.gz"
-  sha256 "5dd2c37f2de67bcf0442818442bfa3c9439ce38d4316dbf9365e3b1732de537e"
+  url "https://github.com/facebook/fb303/archive/refs/tags/v2024.12.30.00.tar.gz"
+  sha256 "6e055a130629990ad08732f175862c058144731004c30907fdb173030fd18b32"
   license "Apache-2.0"
   head "https://github.com/facebook/fb303.git", branch: "main"
 
