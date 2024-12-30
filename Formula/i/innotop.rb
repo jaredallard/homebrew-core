8cf205025c4b192fcc52b8db7a8b1a7fa7c99f4a
Michael Cho <michael@michaelcho.dev>
2024-12-30 14:21:28 -0500

    innotop: use `perl-dbd-mysql`

--- Formula/i/innotop.rb
+++ Formula/i/innotop.rb
@@ -17,26 +17,9 @@ class Innotop < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "4297dcd2edc58800032abfba5b2a89c1983a9ce79b8ca00679343c7b3c34afc4"
   end
 
-  depends_on "mysql-client"
+  depends_on "perl-dbd-mysql"
 
   uses_from_macos "perl"
-
-  resource "Devel::CheckLib" do
-    url "https://cpan.metacpan.org/authors/id/M/MA/MATTN/Devel-CheckLib-1.16.tar.gz"
-    sha256 "869d38c258e646dcef676609f0dd7ca90f085f56cf6fd7001b019a5d5b831fca"
-  end
-
-  resource "DBI" do
-    on_linux do
-      url "https://cpan.metacpan.org/authors/id/H/HM/HMBRAND/DBI-1.645.tgz"
-      sha256 "e38b7a5efee129decda12383cf894963da971ffac303f54cc1b93e40e3cf9921"
-    end
-  end
-
-  resource "DBD::mysql" do
-    url "https://cpan.metacpan.org/authors/id/D/DV/DVEEDEN/DBD-mysql-5.009.tar.gz"
-    sha256 "8552d90dfddee9ef36e7a696da126ee1b42a1a00fbf2c6f3ce43ca2c63a5b952"
-  end
 
   resource "Term::ReadKey" do
     on_linux do
@@ -46,36 +29,19 @@ class Innotop < Formula
   end
 
   def install
-    ENV.prepend_create_path "PERL5LIB", buildpath/"build_deps/lib/perl5"
+    ENV.prepend_path "PERL5LIB", Formula["perl-dbd-mysql"].opt_libexec/"lib/perl5"
     ENV.prepend_create_path "PERL5LIB", libexec/"lib/perl5"
 
     resources.each do |r|
       r.stage do
-        install_base = (r.name == "Devel::CheckLib") ? buildpath/"build_deps" : libexec
-
-        # Skip installing man pages for libexec perl modules to reduce disk usage
-        system "perl", "Makefile.PL", "INSTALL_BASE=#{install_base}", "INSTALLMAN1DIR=none", "INSTALLMAN3DIR=none"
-
-        make_args = []
-        if OS.mac? && r.name == "DBD::mysql"
-          # Reduce overlinking on macOS
-          make_args << "OTHERLDFLAGS=-Wl,-dead_strip_dylibs"
-          # Work around macOS DBI generating broken Makefile
-          inreplace "Makefile" do |s|
-            old_dbi_instarch_dir = s.get_make_var("DBI_INSTARCH_DIR")
-            new_dbi_instarch_dir = "#{MacOS.sdk_path_if_needed}#{old_dbi_instarch_dir}"
-            s.change_make_var! "DBI_INSTARCH_DIR", new_dbi_instarch_dir
-            s.gsub! " #{old_dbi_instarch_dir}/Driver_xst.h", " #{new_dbi_instarch_dir}/Driver_xst.h"
-          end
-        end
-
-        system "make", "install", *make_args
+        system "perl", "Makefile.PL", "INSTALL_BASE=#{libexec}", "INSTALLMAN1DIR=none", "INSTALLMAN3DIR=none"
+        system "make", "install"
       end
     end
 
     system "perl", "Makefile.PL", "INSTALL_BASE=#{prefix}", "INSTALLSITEMAN1DIR=#{man1}"
     system "make", "install"
-    bin.env_script_all_files(libexec/"bin", PERL5LIB: libexec/"lib/perl5")
+    bin.env_script_all_files(libexec/"bin", PERL5LIB: ENV["PERL5LIB"])
   end
 
   test do
